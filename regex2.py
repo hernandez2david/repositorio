72ee7098c918d2f99f4324da5e366d73c3c546f9
hernandez2david <45967368+hernandez2david@users.noreply.github.com>
2019-08-12 15:58:56 -0500

    Rename regex.py to regex2.py

---
+++ regex2.py
@@ -0,0 +1,29 @@
+#! python3
+import re, pyperclip
+#create a regex for phone numbers
+phoneRegex = re.compile(r''' 
+#52 41 45 45 Ext. 2127
+#(998) 8 81 01 00 Ext. 63904
+#(81) 81 58 15 00 Ext. 1503
+#83 54 83 48
+((\(\d\d\d\)) | (\(\d\d\)))? #area code (optional)
+(\s) #Primer separador
+(\d\s\d\d\s\d\d\s\d\d | \d\d\s\d\d\s\d\d\s\d\d) #Formato de numeros
+((ext(\.)?\s |x) (\d{2,5}))? #extension
+'''
+,re.VERBOSE)
+
+#todo: create a regex for email addresses
+
+emailRegex = re.compile(r''' 
+
+
+'''
+,re.VERBOSE)
+   
+
+
+
+#todo: get the text off the clipboard
+#todo: Extract the email / phone from this text
+#todo: copy the extracted email / phone to the clipboard
